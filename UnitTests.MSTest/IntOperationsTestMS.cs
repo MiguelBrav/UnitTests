916df470e55ba876c84d0f47251190602d6b0ba0
MiguelBrav <miguel_98-@hotmail.com>
2024-02-26 21:18:03 -0600

    add list of int tests

---
+++ UnitTests.MSTest/IntOperationsTestMS.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTests.InitialProject.TestExercise;
+
+namespace UnitTests.MSTest
+{
+    [TestClass]
+    public class IntOperationsTestMS
+    {
+        private List<int> numbers;
+        private IntOperations intOperations;
+        private const int MaxRandomNumber = 100;
+        private const int NumbersToGenerate = 10;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            Random rand = new Random();
+            numbers = new List<int>();
+            while (numbers.Count < NumbersToGenerate)
+            {
+                int randomNumber = rand.Next(1, MaxRandomNumber + 1); 
+                if (!numbers.Contains(randomNumber))
+                {
+                    numbers.Add(randomNumber);
+                }
+            }
+            intOperations = new IntOperations();
+        }
+
+        [TestMethod]
+        public void TestFindPrimeNumbers_ReturnsPrimeNumbers()
+        {
+            List<int> primes = intOperations.FindPrimeNumbers(numbers);
+            foreach (int prime in primes)
+            {
+                Assert.IsTrue(intOperations.IsPrime(prime));
+            }
+        }
+
+        [TestMethod]
+        public void TestCalculateFactorial_CalculatesFactorialCorrectly()
+        {
+            foreach (int num in numbers)
+            {
+                long expected = 1;
+                for (int i = 1; i <= num; i++)
+                {
+                    expected *= i;
+                }
+                long result = intOperations.CalculateFactorial(num); 
+                Assert.AreEqual(expected, result);
+            }
+        }
+
+        [TestMethod]
+        public void TestCheckParity_ChecksParityCorrectly()
+        {
+            Dictionary<int, bool> parityResult = intOperations.CheckParity(numbers);
+            foreach (var kvp in parityResult)
+            {
+                int num = kvp.Key;
+                bool isEven = kvp.Value;
+                Assert.AreEqual(num % 2 == 0, isEven); 
+            }
+        } 
+    }
+}
---
+++ UnitTests.XUnit/IntOperationsTestXUnit.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTests.InitialProject.TestExercise;
+using Xunit;
+
+namespace UnitTests.XUnit
+{
+
+    public class IntOperationsTestXUnit
+    {
+        public class IntOperationsTests
+        {
+            private List<int> randomNumbers;
+            private IntOperations intOperations;
+            private const int MaxRandomNumber = 100;
+            private const int NumbersToGenerate = 10;
+
+            public IntOperationsTests()
+            {
+                Random rand = new Random();
+                randomNumbers = new List<int>();
+                while (randomNumbers.Count < NumbersToGenerate)
+                {
+                    int randomNumber = rand.Next(1, MaxRandomNumber + 1);
+                    if (!randomNumbers.Contains(randomNumber))
+                    {
+                        randomNumbers.Add(randomNumber);
+                    }
+                }
+                intOperations = new IntOperations();
+            }
+
+            [Fact]
+            public void TestFindPrimeNumbers_ReturnsPrimeNumbers()
+            {
+                List<int> primeNumbers = intOperations.FindPrimeNumbers(randomNumbers);
+                foreach (int prime in primeNumbers)
+                {
+                    Assert.True(intOperations.IsPrime(prime));
+                }
+            }
+
+            [Fact]
+            public void TestCalculateFactorial_CalculatesFactorialCorrectly()
+            {
+                foreach (int num in randomNumbers)
+                {
+                    long expectedFactorial = 1;
+                    for (int i = 1; i <= num; i++)
+                    {
+                        expectedFactorial *= i;
+                    }
+                    long calculatedFactorial = intOperations.CalculateFactorial(num);
+                    Assert.Equal(expectedFactorial, calculatedFactorial);
+                }
+            }
+
+            [Fact]
+            public void TestCheckParity_ChecksParityCorrectly()
+            {
+                Dictionary<int, bool> parityResult = intOperations.CheckParity(randomNumbers);
+                foreach (var kvp in parityResult)
+                {
+                    int num = kvp.Key;
+                    bool isEven = kvp.Value;
+                    Assert.Equal(num % 2 == 0, isEven);
+                }
+            }
+        }
+    }
+}
